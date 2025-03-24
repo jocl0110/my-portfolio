0b5f63f655ea696edf73dad2affd3c78780a4ea4
Jose Luis Izquierdo Hernandez <joseluisizquierdoshernandez@gmail.com>
2025-03-23 23:33:53 -0500

    Add styles to the footer section

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,15 +1,35 @@
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 import { SideBarStateProps } from "../App";
+import { FaX } from "react-icons/fa6";
 
 function Footer({ isSidebarOpen }: SideBarStateProps) {
   const year = new Date().getFullYear();
   return (
     <footer className={isSidebarOpen ? "sidebar-open" : "sidebar-closed"}>
-      <ul>
-        <li>Twitter</li>
-        <li>LinkedIn</li>
-        <li>Github</li>
-      </ul>
       <p>&copy; Jose Izquierdo {year}</p>
+      <div className="social-icons">
+        <a
+          href="https://github.com/jocl0110"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaGithub />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/jose-luis-izquierdo-hernandez-938064245/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaLinkedin />
+        </a>
+        <a
+          href="https://x.com/jocl0110"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaX />
+        </a>
+      </div>
     </footer>
   );
 }
